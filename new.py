e7a3a068c1c8758f712e24e6ca7651b0dddf28a7
Avinash Reddy <avinash.reddy@v-labs.ai>
2023-03-04 15:01:53 +0530

    random code

--- new.py
+++ new.py
@@ -1,3 +1,13 @@
 import math
 
-print(math.sqrt(25))
+
+def newton_sqrt(x):
+    if x < 0:
+        raise ValueError("Cannot compute square root "
+                         "of negative number {}".format(x))
+    guess = x
+    i = 0
+    while guess * guess != x and i < 20:
+        guess = (guess + x / guess) / 2.0
+        i += 1
+    return guess
